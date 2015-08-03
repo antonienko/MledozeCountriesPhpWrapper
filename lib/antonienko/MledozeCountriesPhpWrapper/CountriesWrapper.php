ddadf8c80f5da04a4a20fc71ede953297c22b60e
Antonio Hernández <antonio.hernandez@panamedia.net>
2015-08-03 17:06:48 +0200

    Get country list in an array

--- lib/antonienko/MledozeCountriesPhpWrapper/CountriesWrapper.php
+++ lib/antonienko/MledozeCountriesPhpWrapper/CountriesWrapper.php
@@ -1,7 +1,20 @@
 <?php
-namespace antonienko\CountriesWrapper;
+namespace antonienko\MledozeCountriesPhpWrapper;
 
 class CountriesWrapper
 {
+    private $jsonData;
+    public function __construct($jsonToLoad = null)
+    {
+        $this->jsonData = json_decode($jsonToLoad ? $jsonToLoad : file_get_contents(__DIR__.'/../../../vendor/mledoze/countries/dist/countries.json'));
+    }
 
+    public function countryList()
+    {
+        $result = [];
+        foreach ($this->jsonData as $country) {
+            $result[] = $country->name->common;
+        }
+        return $result;
+    }
 }
